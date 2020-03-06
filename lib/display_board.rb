b89ffe6309a5b5a7f6e13b8262d979c0a7d82b1a
Abu Rivera <abuarivera@gmail.com>
2020-03-06 07:29:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,3 +13,11 @@ def display_board board = [" "," "," "," "," "," "," "," "," "]
     puts "-----------"
     puts "   |   |   "
 end
+
+def display_board board = [" "," "," "," ","X"," "," "," "," "]
+    puts "   |   |   "
+    puts "-----------"
+    puts "   | X |   "
+    puts "-----------"
+    puts "   |   |   "
+end
