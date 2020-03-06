518cd3185c76168635ce2a71d42bd29bb206bf65
Abu Rivera <abuarivera@gmail.com>
2020-03-06 07:49:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,63 +1,7 @@
-def display_board
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-end
-
 def display_board board = [" "," "," "," "," "," "," "," "," "]
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
-
-def display_board board = [" "," "," "," ","X"," "," "," "," "]
-    puts "   |   |   "
-    puts "-----------"
-    puts "   | X |   "
-    puts "-----------"
-    puts "   |   |   "
-end
-
-def display_board board = ["O"," "," "," ","X"," "," "," "," "]
-    puts " O |   |   "
-    puts "-----------"
-    puts "   | X |   "
-    puts "-----------"
-    puts "   |   |   "
-end
-
-def display_board board = ["X","X","X"," "," "," "," "," "," "]
-    puts " X | X | X "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-end
-
-def display_board board = [" "," "," "," "," "," ","O","O","O"]
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts " O | O | O "
-end
-
-def display_board board = ["X"," "," "," ","X"," "," "," ","X"]
-    puts " X |   |   "
-    puts "-----------"
-    puts "   | X |   "
-    puts "-----------"
-    puts "   |   | X "
-end
-
-def display_board board = ["O"," "," "," ","O"," "," "," ","O"]
-    puts "   |   | O "
-    puts "-----------"
-    puts "   | O |   "
-    puts "-----------"
-    puts " O |   |   "
-end  
